8794ee4ebb1e9a2954584674a503312faf8df34b
Manuchei <72012109+Manuchei@users.noreply.github.com>
2020-10-24 12:01:26 +0200

    Add files via upload

---
+++ Ejercicio6.psc
@@ -0,0 +1,11 @@
+Algoritmo Ejercicio6
+	Definir existencias como entero
+	Definir entregadas  Como Entero
+	existencias <- 3200
+	Mientras existencias>=100 Hacer
+		Escribir 'Selecciona el nº de existencias a pedir'
+		Leer entregadas
+		existencias <- existencias+1
+		Escribir 'existencias actuales',existencias
+	FinMientras
+FinAlgoritmo
---
+++ ejercicio6.java
@@ -0,0 +1,22 @@
+package yo;
+
+import java.util.Scanner;
+
+public class ejercicio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int existencias = 3200;
+		Scanner reader = new Scanner (System.in);
+		int entregados = 0;
+		System.out.println("entregadas");
+		while(existencias>-100);
+				System.out.println("seleccion nº de existencias");
+	            System.out.println("La diferencia existencias - entregados=" + (existencias-entregados));
+		        System.out.println("existencias actuales= "+existencias);
+		        System.out.println("Existencias actuales=" + existencias);
+		        System.out.println("No existencias disponibles");
+	}
+
+}
